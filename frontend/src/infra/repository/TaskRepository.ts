7537f78e5048ff5b176123ed47e9ae2773617330
giselaortt <ms.giselaortt@gmail.com>
2025-11-14 02:38:22 +0100

    properly connecting the backend with the front end

--- frontend/src/infra/repository/TaskRepository.ts
+++ frontend/src/infra/repository/TaskRepository.ts
@@ -1,26 +1,35 @@
 import type { Task } from "../../domain/task"
 
 export class TaskRepository {
-	private readonly baseUrl = "http://localhost:3002/api/tasks"
+	private readonly baseUrl = "http://localhost:3000/api/tasks" // Changed from 3002 to 3000
 
 	getAll = async () => {
 		const tasks = await fetch(this.baseUrl)
 		return tasks.json()
 	}
+	
 	create = async (task: Omit<Task, "id">) => {
 		const response = await fetch(this.baseUrl, {
 			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
 			body: JSON.stringify(task),
 		})
 		return response.json()
 	}
+	
 	update = async (id: string, task: Partial<Task>) => {
 		const response = await fetch(`${this.baseUrl}/${id}`, {
 			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+			},
 			body: JSON.stringify(task),
 		})
 		return response.json()
 	}
+	
 	delete = async (id: string) => {
 		const response = await fetch(`${this.baseUrl}/${id}`, {
 			method: "DELETE",
